76ce0c3e33907105803c9150816f8fbcf5449526
zeigous <153451177+thatonepcguy@users.noreply.github.com>
2024-09-14 19:04:58 -0600

    Update boids.py

--- boids.py
+++ boids.py
@@ -24,10 +24,20 @@ class Boid(pygame.sprite.Sprite):
         self.position =     pygame.Vector2(random.randint(0, 1280), random.randint(0, 720))
         self.velocity =     pygame.Vector2(random.uniform(-10, 10), random.uniform(-10, 10))
         self.acceleration = pygame.Vector2(0, 0)
+
+        self.type = random.choice(["blue", "red", "gray"])
+
         self.speed =        0
 
-        self.maxSpeed =     10
-        self.minSpeed =     7
+        if self.type == "blue":
+            self.maxSpeed =     10
+            self.minSpeed =     7
+        elif self.type == "red":
+            self.maxSpeed =     8
+            self.minSpeed =     5
+        elif self.type == "gray":
+            self.maxSpeed =     12
+            self.minSpeed =     9
 
         self.mass = random.uniform(1.0,2.0)
 
@@ -94,7 +104,7 @@ class Boid(pygame.sprite.Sprite):
         rotatedPoints = [[trianglePoints[0][0]*math.cos(rotation)-trianglePoints[0][1]*math.sin(rotation)+self.position.x, trianglePoints[0][0]*math.sin(rotation)+trianglePoints[0][1]*math.cos(rotation)+self.position.y],
                          [trianglePoints[1][0]*math.cos(rotation)-trianglePoints[1][1]*math.sin(rotation)+self.position.x, trianglePoints[1][0]*math.sin(rotation)+trianglePoints[1][1]*math.cos(rotation)+self.position.y],
                          [trianglePoints[2][0]*math.cos(rotation)-trianglePoints[2][1]*math.sin(rotation)+self.position.x, trianglePoints[2][0]*math.sin(rotation)+trianglePoints[2][1]*math.cos(rotation)+self.position.y]]
-        pygame.draw.polygon(self.screen, (155,155,155), rotatedPoints)
+        pygame.draw.polygon(self.screen, self.type, rotatedPoints)
 
 
 ##################
@@ -106,7 +116,7 @@ def alignment(boids):
         count = 0
         
         for boid2 in boids:
-            if boid1 != boid2:
+            if boid1 != boid2 and boid1.type == boid2.type:
                 distance = boid1.position.distance_to(boid2.position)
                 if distance < visualRange:    
                     avgVelocity += boid2.velocity
@@ -122,7 +132,7 @@ def cohesion(boids):
         netPos = pygame.Vector2(0,0)
         count = 0
         for boid2 in boids:
-            if boid1 != boid2:
+            if boid1 != boid2 and boid1.type == boid2.type:
                 distance = boid1.position.distance_to(boid2.position) 
                 if distance < visualRange:
                     netPos += boid2.position
